b1fe67c25f4941ecd6a9d542385e79c647b6c494
niushengjun <abc12345fyhj@gmail.com>
2025-06-09 20:29:18 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
+USER_NAME = "niu"         # 用户名
 UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "niu.kk-jun-sheng.xyz"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiNDllZWZmMzkwNWI1YWZiMDlmYTljZmY2OTk2YzBiNmMiLCJ0IjoiYzE5ZTlmNTUtNjhhNi00ZDYyLWJhZWEtZGU3NDZkOTg5MDAwIiwicyI6Ik1ESTBZelV4WmpndE5UTmxPQzAwT0dRNUxXSXlabVl0TVRGak16Z3dZV1kzTnpNMSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
